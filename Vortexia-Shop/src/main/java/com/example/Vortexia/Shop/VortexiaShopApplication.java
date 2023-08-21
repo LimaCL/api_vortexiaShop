3d007401a7b1e27380f781715c11152e753d5987
LimaCL <113440100+LimaCL@users.noreply.github.com>
2023-08-21 08:40:09 -0300

    Criação do Projeto

---
+++ Vortexia-Shop/src/main/java/com/example/Vortexia/Shop/VortexiaShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.Vortexia.Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VortexiaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VortexiaShopApplication.class, args);
+	}
+
+}
---
+++ Vortexia-Shop/src/test/java/com/example/Vortexia/Shop/VortexiaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Vortexia.Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VortexiaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
